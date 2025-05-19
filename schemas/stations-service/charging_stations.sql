0421013f7630e486b171889b4310825c6900d002
Y.T. <sasaovch@gmail.com>
2025-05-19 11:18:41 +0300

    Небольшие правки

--- schemas/stations-service/charging_stations.sql
+++ schemas/stations-service/charging_stations.sql
@@ -7,8 +7,8 @@ CREATE TABLE charging_stations (
     location GEOGRAPHY(Point, 4326) NOT NULL,
     occupancy_ratio NUMERIC(3, 2) CHECK (occupancy_ratio >= 0 AND occupancy_ratio <= 1), -- от 0 до 1
     price_per_kwh NUMERIC(10, 2),
-    operator TEXT,
-    manufacturer TEXT,
+    provider_id INTEGER REFERENCES providers(id) ON SET NULL,
+    manufacturer_id INTEGER REFERENCES manufacturers(id) ON SET NULL,
     status VARCHAR(30) NOT NULL, -- 'available', 'offline', 'busy'
     service_types TEXT,          -- 'cafes', 'shops', 'parks'
     created_at TIMESTAMP NOT NULL DEFAULT NOW(),
--- schemas/user-service/connectors.sql
+++ schemas/user-service/connectors.sql
@@ -1,4 +1,4 @@
-CREATE TABLE car_connectors (
+CREATE TABLE connectors (
     id SERIAL PRIMARY KEY,
     connector_type VARCHAR(50) NOT NULL -- Тип разъема (например, CCS, CHAdeMO, Type 2)
 );
